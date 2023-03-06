53108df3e954dea6e3cc4ea37be56b93063627f0
stevenquintana <steve.e.quint@gmail.com>
2023-03-06 15:00:03 -0800

    Complete dequeue function and execution portion

--- algorithms-datastructures/basic-intermediate/practice/stacks-queues/callerHoldQueue.ts
+++ algorithms-datastructures/basic-intermediate/practice/stacks-queues/callerHoldQueue.ts
@@ -3,15 +3,36 @@
 class Queue {
   private data: string[] = []
 
+  public size: number = 0;
+
   constructor() {}
 
-  // Adds a passed value to the beginning of the queue data array
+  // Adds a passed value to the end of the queue data array
   enqueue(value: string): void {
-    this.data.unshift(value)
+    this.size++
+    this.data.push(value)
   }
 
   // Removes and returns the first value of the data array
   dequeue() {
+    this.size--
     return this.data.shift()
   }
 }
+
+// Queue execution 
+  const callerQueue: Queue = new Queue()
+  const firstCaller: string = 'Debra'
+  const secondCaller: string = 'Jason'
+  const thirdCaller: string = 'Joy'
+
+  // Add the callers to the queue
+  callerQueue.enqueue(firstCaller)
+  callerQueue.enqueue(secondCaller)
+  callerQueue.enqueue(thirdCaller)
+
+  // While the queue has callers in it, dequeue each caller (take the calls)
+  // Print the callers as the calls are taken
+  while (callerQueue.size > 0) {
+    console.log(callerQueue.dequeue())
+  }

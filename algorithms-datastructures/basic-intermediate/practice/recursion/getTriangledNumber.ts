6225227e19a1028d297547a0fa328ee1dfe42dff
stevenquintana <steve.e.quint@gmail.com>
2023-03-10 05:57:33 -0800

    Create a function that takes in a number and returns it's triangled value using recursion

--- algorithms-datastructures/basic-intermediate/practice/recursion/getTriangledNumber.ts
+++ algorithms-datastructures/basic-intermediate/practice/recursion/getTriangledNumber.ts
@@ -3,18 +3,13 @@
 // The result should be the position + the previous value in the array
 // Time Complexity: O(N)
 
-const getTriangledNumber = (position: number, numbers: number[] = [0]): number => {
-  console.log(position)
-  console.log(numbers)
-  // Base case: if the length of the array is equal to the position, this is the target value
-  if (numbers.length > 0 && position === numbers.length) {
-    console.log(numbers[numbers.length - 1] + position)
-    return numbers[numbers.length - 1] + position
+const getTriangledNumber = (target: number, position: number = 0, priorValue: number = 0): number => {
+  // Base case
+  if (position === target) {
+    return position + priorValue
   }
 
-  // Pass the value from base case back to the top of the call stack
-  // The second arg represents the new triangled number added to the numbers array
-  return getTriangledNumber(position, [...numbers, position + numbers[numbers.length - 2]])
+  return getTriangledNumber(target, position + 1, position + priorValue)
 }
 
 // Execution script
